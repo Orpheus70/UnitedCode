d7addd9876b3ea6b2ccd1d156c384e42a672a6fb
Steapan0606 <steapan0606@gmail.com>
2025-05-30 15:52:04 +0700

    3

--- main.cpp
+++ main.cpp
@@ -1,25 +1,4 @@
-#include <stdio.h>
-#include <windows.h>
-
-// Макрос для импорта функции из DLL
-#ifdef _WIN32
-#define IMPORT __declspec(dllimport)
-#else
-#define IMPORT
-#endif
-
-typedef void (*ResultCallback)(int status, const char* d);
-
-// Временная реализация send_command для теста
-int send_command(const char* d, ResultCallback callback_result) {
-    callback_result(0, d);
-    return 0;
-}
-
-// Реализация callback-функции
-void callback_result(int status, const char* d) {
-    printf("%d %s\n", status, d);
-}
+#include "examplePort.cpp"
 
 int main() {
     send_command("123", callback_result);
